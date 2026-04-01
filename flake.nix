0b3977319ae326532deafb28092ae69594d9b3df
Walrus <jrag137@gmail.com>
2026-04-01 19:46:15 +0530

    initial config

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+    description = "Walrus's NixOS";
+    inputs = {
+        nixpkgs.url = "nixpkgs/nixos-25.05";
+        home-manager = {
+            url = "github:nix-community/home-manager/release-25.05";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, nixpkgs, home-manager, ... }: {
+        nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
+            system = "amd64-linux";
+            modules = [
+                ./configuration.nix
+                home-manager.nixosModules.home-manager
+                {
+                    home-manager = {
+                        useGlobalPkgs = true;
+                        useUserPackages = true;
+                        users.walrus = import ./home.nix;
+                        backupFileExtension = "backup";
+                    }
+                };
+            ];
+        };
+    };
+
+}
