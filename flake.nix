574933cf18e6ff9f8eb4bf66deee14cb01af85b3
walrus <walrus@walrus.walrus>
2026-04-02 13:24:31 +0530

    made using ssh from VM

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
     inputs = {
         nixpkgs.url = "nixpkgs/nixos-25.05";
         home-manager = {
-            url = "github:nix-community/home-manager/release-25.05";
+            url = "github:nix-community/home-manager/release-25.04";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
